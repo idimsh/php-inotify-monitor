de1bc5daae83ec5b258eaa73ccedde7755858c49
Abdulrahman Dimashki <idimsh@gmail.com>
2018-11-30 00:57:35 +0100

    Added more test coverage.

--- tests/MonitorTest.php
+++ tests/MonitorTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dimsh\React\Filesystem\Monitor\Monitor;
+
 class MonitorTest extends \PHPUnit\Framework\TestCase
 {
     public function testSetup()
@@ -34,5 +36,88 @@ class MonitorTest extends \PHPUnit\Framework\TestCase
           $monitor = new \Dimsh\React\Filesystem\Monitor\Monitor($monitor_config));
 
         $this->assertEquals(null, $monitor->run());
+    }
+
+    public function testFireEvents()
+    {
+        $base_directory = '/tmp/___non___existant___dir___';
+
+        $monitor_config = \Dimsh\React\Filesystem\Monitor\MonitorConfigurator::factory()
+          ->setLevel(3)
+          ->setAutoCreateNotFoundMonitor(true)
+                    ->setFireModifiedOnDirectories(true)
+          ->setFilesToMonitor([
+            '*',
+            '',
+            'dir1-at-lev1/',
+            '/stop-if-created/',
+          ]);
+        try {
+            $monitor_config->setBaseDirectory($base_directory);
+        } catch (\Exception $e) {
+            $this->fail("exception thrown: [{$e->getMessage()}]");
+        }
+
+        $list_of_created_items =
+        $list_of_modified_items =
+        $list_of_deleted_items = [];
+
+        $monitor = new Monitor($monitor_config);
+        $monitor
+          ->on(Monitor::EV_CREATE, function ($path, $monitor) use (&$list_of_created_items, $base_directory) {
+              /** @var Monitor $monitor */
+
+              $list_of_created_items[] = $path;
+              fputs(STDERR, "created:   $path\n");
+              if ($path === "$base_directory/stop-if-created/") {
+                  fputs(STDERR, "stopping ... \n");
+                  $monitor->stop();
+              }
+          })
+          ->on(Monitor::EV_MODIFY, function ($path, $monitor) use (&$list_of_modified_items) {
+              $list_of_modified_items[] = $path;
+              fputs(STDERR, "modified:  $path\n");
+          })
+          ->on(Monitor::EV_DELETE, function ($path, $monitor) use (&$list_of_deleted_items) {
+              $list_of_deleted_items[] = $path;
+              fputs(STDERR, "deleted:   $path\n");
+          });
+
+        $creator_runner_script = __DIR__ . '/helpers/creator-background-runner.sh';
+
+        $pid = exec('bash ' . $creator_runner_script);
+
+        if ($pid && is_numeric($pid)) {
+            // this will block until stop is called in the "oncreate" event handler.
+            $monitor->run();
+
+            $this->assertEquals($list_of_created_items, [
+              "$base_directory/",
+              "$base_directory/file1-at-lev1",
+              "$base_directory/dir1-at-lev1/",
+              "$base_directory/dir1-at-lev1/file11-at-lev2",
+              "$base_directory/dir1-at-lev1/dir11-at-lev2/file111-at-lev3",
+              "$base_directory/dir3-at-lev1/dir11-at-lev2/file111-at-lev3",
+              "$base_directory/dir3-at-lev1/file11-at-lev2",
+              "$base_directory/dir2-at-lev1/file21-at-lev2",
+              "$base_directory/stop-if-created/",
+            ]);
+
+            $this->assertEquals($list_of_modified_items, [
+              "$base_directory/file1-at-lev1",
+              "$base_directory/dir1-at-lev1/",
+            ]);
+
+            $this->assertEquals($list_of_deleted_items, [
+              "$base_directory/dir1-at-lev1/dir11-at-lev2/file111-at-lev3",
+              "$base_directory/dir1-at-lev1/file11-at-lev2",
+              "$base_directory/dir1-at-lev1/",
+              "$base_directory/dir3-at-lev1/dir11-at-lev2/file111-at-lev3",
+              "$base_directory/dir3-at-lev1/file11-at-lev2",
+            ]);
+
+        } else {
+            $this->fail('can not run the creator runner script');
+        }
     }
 }
